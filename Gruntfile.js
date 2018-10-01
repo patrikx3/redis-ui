3288870e0f39d9cf6a41f937ef5bb0c7f34c2388
patrikx3 <alabard@gmail.com>
2018-10-01 14:03:10 +0200

    robo8x [chore]   10/1/2018, 2:03:10 PM

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,10 @@ module.exports = (grunt) => {
     const builder = require(`corifeus-builder`);
     const loader = new builder.loader(grunt);
     loader.js({
+        replacer: {
+            type: 'p3x',
+            npmio: true,
+        },
     });
 
 
