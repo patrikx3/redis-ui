cefb272ce880b077c6d331f3e713068e53d9da3a
patrikx3 <alabard@gmail.com>
2019-08-01 15:28:01 +0200

    robo8x [chore]   8/1/2019, 3:28:01 PM

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,18 +13,6 @@ module.exports = (grunt) => {
         },
     });
 
-
-    const test = grunt.config.get('mocha_istanbul')
-    test['cory-coverage'].src  = [
-        [
-            `${process.cwd()}/src/electron/**/*.js` ,
-        ],
-    ]
-//    test['cory-coverage'].options.mask = `${process.cwd()}/src/electron/**/*.js`
-    test['cory-coverage'].options.includes = [
-        `${process.cwd()}/src/electron/**/*.js` ,
-    ]
-    grunt.config.set('mocha_istanbul', test)
 //console.log(JSON.stringify(test, null, 4));
     let defaults = builder.config.task.build.js.slice()
 //    defaults.splice(defaults.indexOf('mocha_istanbul:cory-coverage'), 1)
