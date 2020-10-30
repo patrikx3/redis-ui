eb3422552d5c6e7eaf97777bc47dbe88e014cd6a
patrikx3 <alabard@gmail.com>
2020-10-30 21:33:34 +0100

    r0b08x [chore]   10/30/2020, 9:33:34 PM

--- src/app.js
+++ src/app.js
@@ -7,71 +7,77 @@ process.on('uncaughtException', function (err) {
     process.exit(-1)
 });
 
-let findFreePort = require('find-port-free-sync');
-let available = false;
-const maxTries = 100
-let tries = 0
-do {
-    try {
-        tries++
-        global.p3xrsElectronPort = findFreePort({
-            start: 10000,
-            end: 60000
-        });
-        console.log('trying open port', global.p3xrsElectronPort)
-        available = true
-    } catch(e) {
-        console.warn(e)
+const execAsync = async() => {
+    let getPort = require('get-port');
+    let available = false;
+    const maxTries = 100
+    let tries = 0
+    do {
+        try {
+            tries++
+            const min = 10000
+            const max = 60000
+            const port = Math.floor(Math.random() * (max - min + 1) + min);
+            global.p3xrsElectronPort = await getPort({
+                port: port
+            });
+            console.log('found open port', global.p3xrsElectronPort)
+            available = true
+        } catch(e) {
+            console.warn(e)
+        }
+    } while (!available && tries < maxTries)
+
+    if (!available) {
+        throw new Error(`Could not find an open port by trying ${maxTries}.`)
     }
-} while (!available && tries < maxTries)
 
-if (!available) {
-    throw new Error(`Could not find an open port by trying ${maxTries}.`)
-}
+    const boot = require('p3x-redis-ui-server/src/lib/boot')
+    boot()
 
-const boot = require('p3x-redis-ui-server/src/lib/boot')
-boot()
+    require('./electron/global.js');
 
-require('./electron/global.js');
-
-const {app, ipcMain} = require('electron')
+    const {app, ipcMain} = require('electron')
 
 //app.commandLine.appendSwitch("ignore-certificate-errors");
 
-if ((process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test')) {
-    app.commandLine.appendSwitch('remote-debugging-port', '9222')
-}
+    if ((process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test')) {
+        app.commandLine.appendSwitch('remote-debugging-port', '9222')
+    }
 
-const gotTheLock = app.requestSingleInstanceLock()
+    const gotTheLock = app.requestSingleInstanceLock()
 
-if (!gotTheLock) {
-    app.quit()
-    return
-}
-app.on('second-instance', (event, commandLine, workingDirectory) => {
-    // Someone tried to run a second instance, we should focus our window.
-    global.p3xre.setVisible(true);
-    //global.p3xre.mainWindow.webContents.reload();
-})
+    if (!gotTheLock) {
+        app.quit()
+        return
+    }
+    app.on('second-instance', (event, commandLine, workingDirectory) => {
+        // Someone tried to run a second instance, we should focus our window.
+        global.p3xre.setVisible(true);
+        //global.p3xre.mainWindow.webContents.reload();
+    })
 
-const createWindow = require('./electron/module/create/window');
+    const createWindow = require('./electron/module/create/window');
 
-app.on('ready', () => {
-    createWindow();
+    app.on('ready', () => {
+        createWindow();
 
 
 //    ngivrSession.init();
-});
+    });
 
-app.on('window-all-closed', function () {
+    app.on('window-all-closed', function () {
 
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
+        if (process.platform !== 'darwin') {
+            app.quit();
+        }
+    });
 
-app.on('activate', function () {
-    if (global.p3xre.mainWindow === null) {
-        createWindow();
-    }
-});
+    app.on('activate', function () {
+        if (global.p3xre.mainWindow === null) {
+            createWindow();
+        }
+    });
+
+}
+execAsync()
